57d2a23d156f17f7c40a2917c6df876d7d2e1dfb
erfan <mahdi.farahmandsafa@gmail.com>
2025-10-03 12:38:43 +0330

    create order items table

---
+++ db/migrations/000013_create_order_items_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity SMALLINT NOT NULL,
+    price NUMERIC(13, 3) NOT NULL,
+    created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE SET NULL,
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE SET NULL
+);
