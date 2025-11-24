82760adce903aed5c6b8a05d7fbabd93e2cb4f8a
messmer <messmer@debian.debian>
2025-11-25 00:14:08 +0330

    set product types , categories and brands image field in models as pointer or allowed to be null

--- models/category.go
+++ models/category.go
@@ -4,5 +4,5 @@ type Category struct {
 	ID         int    `json:"id" db:"id"`
 	Title      string `json:"title" db:"title"`
 	TitleFarsi string `json:"title_farsi" db:"title_farsi"`
-	Image      string `json:"image" db:"image"`
+	Image      *string `json:"image" db:"image"`
 }
--- models/productType.go
+++ models/productType.go
@@ -5,5 +5,5 @@ type ProductType struct {
 	CategoryID int    `json:"category_id" db:"category_id"`
 	Title      string `json:"title" db:"title"`
 	TitleFarsi string `json:"title_farsi" db:"title_farsi"`
-	Image      string `json:"image" db:"image"`
+	Image      *string `json:"image" db:"image"`
 }
