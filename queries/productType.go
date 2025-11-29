24c3d294aee98fb705e12b17fa4c0d81c34fe799
erfan <mahdi.farahmandsafa@gmail.com>
2025-11-29 18:01:08 +0330

    filter only product types that their image are not null and order them by created time

--- queries/productType.go
+++ queries/productType.go
@@ -2,7 +2,9 @@ package queries
 
 const (
 	ProductTypeList = `
-		SELECT id,category_id,title,title_farsi,image FROM product_types;
+		SELECT id,category_id,title,title_farsi,image FROM product_types
+		WHERE image IS NOT NULL
+		ORDER BY created;
 	`
 
 	ProductTypesBasedOnCategory = `
