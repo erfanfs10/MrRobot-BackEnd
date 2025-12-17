212f813a2dff19fb6517b45509e231e4630e91b4
erfan <mahdi.farahmandsafa@gmail.com>
2025-12-17 10:47:25 +0330

    get user_id from Authorization header in production

--- middlewares/authenticate.go
+++ middlewares/authenticate.go
@@ -1,37 +1,26 @@
 package middlewares
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/erfanfs10/MrRobot-BackEnd/utils"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
 
 func Authenticate() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			userID := c.Request().Header.Get("user_id")
-			fmt.Println(userID, "userID")
-			auth := c.Request().Header.Get("Authorization")
-			tokenStr := strings.TrimPrefix(auth, "Bearer ")
-			fmt.Println(auth, "auth")
-			fmt.Println(tokenStr, "tokenStr")
-			sess := c.Request().Header.Get("session")
-			fmt.Println(sess, "sss")
-			sec := utils.GetEnv("DB_USER")
-			token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
-				return []byte(sec), nil
-			})
 
-			if err != nil || !token.Valid {
-				fmt.Println(err)
+			var userID string
+
+			isProduction := utils.GetEnv("IS_PRODUCTION")
+			if isProduction == "yes" {
+				auth := c.Request().Header.Get("Authorization")
+				userID = strings.TrimPrefix(auth, "Bearer ")
+			} else {
+				userID = c.Request().Header.Get("user_id")
 			}
-
-			claims := token.Claims.(jwt.MapClaims)
-			fmt.Println(claims, "cccc")
-
+			
 			if userID == "" {
 				return echo.ErrUnauthorized
 			}
